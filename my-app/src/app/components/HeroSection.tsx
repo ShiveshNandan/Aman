fe6331c63b38701d64180988bcce61216958bd3a
Shivesh Nandan <kshivesh929@gmail.com>
2024-01-25 14:38:49 +0530

    padding

--- my-app/src/app/components/HeroSection.tsx
+++ my-app/src/app/components/HeroSection.tsx
@@ -5,18 +5,23 @@ const HeroSection = () => {
     <div className="flex flex-col w-10/12  mx-auto bg-blue-600 text-white ">
       <div className="flex my-5">
         <div className="flex w-1/2 flex-col my-auto px-3">
-          <h1 className="font-semibold text-6xl leading-[4rem]">
+          <h1 className="font-semibold text-6xl leading-[4.5rem]">
             Create your next mobile app landing page in minutes
           </h1>
           <p className="text-sm w-8/12 py-5">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum quo,
             nihil perspiciatis ipsa modi esse fugit odit repellendus quae saepe.
           </p>
+          <div className="mt-5 flex w-8/12">
           <input
             type="text"
-            className="mt-5 rounded-full w-8/12 py-3 px-5 text-sm"
+            className=" rounded-l-full py-3 px-5 text-sm text-black inline:none focus:outline-none "
             placeholder="Search"
           />
+          <div className="flex bg-white rounded-r-full ">
+          <button className="my-auto bg-blue-500 rounded-full h-9  px-3 mx-1">Fredays</button>
+          </div>
+          </div>
         </div>
         <div className="flex w-1/2 h-[60vh] bg-blue-500">
             <img src="" alt="" className="w-2/3 h-full bg-black mx-auto" />
